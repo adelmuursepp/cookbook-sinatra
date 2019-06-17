954a468a330cb2d2fdfae007d1fd26652e109488
Adel Muursepp <adel.muursepp@gmail.com>
2019-06-17 20:03:12 +0300

    Website works, CSS not though

--- app.rb
+++ app.rb
@@ -5,20 +5,63 @@ require "better_errors"
 require_relative "./files/cookbook.rb"
 require_relative "./files/parsing.rb"
 require_relative "./files/recipe.rb"
-require_relative "./DB/recipes.csv"
 set :bind, '0.0.0.0'
+
+configure do
+  set :public_folder, File.dirname(__FILE__) + '/static'
+end
 
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
-csv_file = "./DB/recipes.csv"
+csv_file = File.join(__dir__, "DB/recipes.csv")
 cookbook = Cookbook.new(csv_file)
+parser = Parsing.new
 
 get "/" do
   @recipes = cookbook.all
   erb :index
 end
 
+get "/new" do
+  erb :new
+end
 
+post "/" do
+  name = params["name"]
+  description = params["description"]
+  cookingtime = params["prep time"]
+  recipe = Recipe.new(name, description, cookingtime)
+  cookbook.add_recipe(recipe)
+  @recipes = cookbook.all
+  erb :index
+end
+
+get "/destroy/:index" do
+  index = params[:index]
+  cookbook.remove_recipe(index.to_i)
+  redirect "/"
+end
+
+get "/mark/:index" do
+  index = params[:index]
+  cookbook.mark_as_done(index.to_i)
+  redirect "/"
+end
+
+get "/inspiration" do
+  erb :inspiration
+end
+
+post "/inspiration" do
+  keyword = params["keyword"]
+  redirect "/ideas/#{keyword}"
+end
+
+get "/ideas/:keyword" do
+  keyword = params[:keyword]
+  @ideas = parser.scrape(keyword)
+  erb :ideas
+end
